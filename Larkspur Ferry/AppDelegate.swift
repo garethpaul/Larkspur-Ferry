67a9b42c3058610b9a337b3342ca9ea66e513d03
Gareth Jones <gareth@garethpaul.com>
2016-05-02 20:00:00 -0600

    add changes to appdelegate

--- Larkspur Ferry/AppDelegate.swift
+++ Larkspur Ferry/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         // Setup Uber SDK
-        RidesClient.sharedInstance.configureClientID("WzfJJPRAuPgm9KNlyraTn0KY8OLY3efX")
+        RidesClient.sharedInstance.configureClientID("AAAAAAAAAAAAAAAAA")
         return true
     }
 
