8f4298d4dee4cbcc8f0d8d4e5260ce2bc993aa24
Gareth Jones <gareth@garethpaul.com>
2016-05-05 20:00:00 -0600

    add changes to project

--- Larkspur Ferry/FerryCell.swift
+++
@@ -1,9 +0,0 @@
-// FerryCell.swift
-import UIKit
-
-
-class FerryCell: UITableViewCell {
-    @IBOutlet weak var arrivalLabel: UILabel!
-    @IBOutlet weak var toLocation: UILabel!
-    @IBOutlet weak var startTime: UILabel!
-}
--- Larkspur Ferry/PinAnnotation.swift
+++
@@ -1,7 +0,0 @@
-import MapKit
-import Foundation
-import UIKit
-
-class CustomPointAnnotation: MKPointAnnotation {
-    var imageName: String!
-}
